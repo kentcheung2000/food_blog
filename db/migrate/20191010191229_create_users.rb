3141a8f7b1fba6d08a71061aa24175fcc9c56914
kentcheung2000 <kentcheung2000@yahoo.com>
2019-10-10 12:18:51 -0700

    Created user model

---
+++ db/migrate/20191010191229_create_users.rb
@@ -0,0 +1,16 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :password_digest
+      t.boolean :is_admin
+      t.string :address
+      t.float :latitude
+      t.float :longitude
+
+      t.timestamps
+    end
+  end
+end
