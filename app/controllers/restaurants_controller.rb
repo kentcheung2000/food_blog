04313543ab5464bec12becc9edb1b797c1ce99d4
kentcheung2000 <kentcheung2000@yahoo.com>
2019-10-22 10:05:38 -0700

    Test to add restaurant and restaurant food

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,36 @@
 class RestaurantsController < ApplicationController
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def index
+    @restaurants = Restaurant.order(created_at: :desc)
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  private
+
+
+
+
+
+
+
+
+
 end
